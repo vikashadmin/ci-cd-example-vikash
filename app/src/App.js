0fd60f12187e51da09b47c0b5137aed156cd18b3
vikash <vikash32@gmail.com>
2022-09-26 13:02:51 +0530

    my 1 commit

--- app/src/App.js
+++ app/src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <div className="title">
-          I am an Example App Running in the Cloud1
+          I am an admin Example App Running in the admin1
         </div>
       </div>
     );
